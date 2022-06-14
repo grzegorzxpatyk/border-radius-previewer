f35bb269f6cb2772d29848da410debe6b5b81a51
grzegorzxpatyk <grzegorz.p4tyk@gmail.com>
2022-06-14 21:34:01 +0200

    add slider selector, eventListener on input event, and the event handler

--- App.js
+++ App.js
@@ -1,1 +1,9 @@
 const previewer = document.querySelector('.previewer');
+const slider = document.querySelector('#border-radius-slider');
+
+function handleSliderChange() {
+    console.log(this.value);
+    previewer.style.borderRadius = `${this.value}%`;
+}
+
+slider.addEventListener('input', handleSliderChange);
